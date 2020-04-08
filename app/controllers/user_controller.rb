4ffc6d17967fa7a9e75fbd05a607a7f1fadcfe50
Lucas Debouté <lucas.deboute@gmail.com>
2020-04-08 16:17:43 +0200

    'user/create' can add a username in database if it is not already registered, returns 500 otherwise.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,21 @@
 class UserController < ApplicationController
     def create
-        render status: :not_implemented
+        @users = User.order("username ASC").pluck(:username)
+        @index = @users.index(params[:username])
+        @response = {}
+        if @index == nil
+            @user = User.new(:username => params[:username])
+            @response = {"status": "OK"}
+        end
+        
+        # TODO 8/4/2020: In case of username already in use, generate a new one here
+
+        if @user != nil and @user.save
+            render json: @response, status: :ok
+        elsif @user != nil and @user.errors != nil
+            render json: {"code": 406, "error": @user.errors.messages }, status: :not_acceptable
+        else
+            render json: {"code": 500 }, status: :internal_server_error
+        end
     end
 end
